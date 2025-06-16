df40e8a059823c09c2e3f51ba7247b7ee36fb106
WebLoxic <workwebloxic@gmail.com>
2025-06-16 16:26:00 +0530

    new

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
 import userModel from '../models/userModel.js';
 import businessModel from '../models/businessModel.js';
 
@@ -11,6 +13,7 @@ const createToken = (id) => {
 }
 
 
+// Generating Referral Code 
 const generateReferralCode = async () => {
     let code;
     let exists = true;
@@ -23,120 +26,10 @@ const generateReferralCode = async () => {
     return code;
 };
 
-
-// const registerUser = async (req, res) => {
-//     try {
-//         const { name, email, password, phone, address, pinCode, district , city, country , DOB , PAN , accountNumber , IFSC  , bankName , holderName ,  inviterefferal } = req.body;
-
-        
-
-
-//         if (!name) {
-//             return res.json({ success: false, message: 'Name is required' });
-//         }
-//         if (!email) {
-//             return res.json({ success: false, message: 'Email is required' });
-//         }
-//         if (!password) {
-//             return res.json({ success: false, message: 'Password  is required' });
-//         }
-//         if (!phone) {
-//             return res.json({ success: false, message: 'Phone  is required' });
-//         }
-//         if (!address) {
-//             return res.json({ success: false, message: 'Address  is required' });
-//         }
-//         if (!pinCode) {
-//             return res.json({ success: false, message: 'Pincode is required' });
-//         }
-//         if (!city) {
-//             return res.json({ success: false, message: 'City is required' });
-//         }
-//         if (!district ) {
-//             return res.json({ success: false, message: 'District  is required' });
-//         }
-//          if (!country ) {
-//             return res.json({ success: false, message: 'Country  is required' });
-//         }
-
-//          if (!DOB) {
-//             return res.json({ success: false, message: 'DOB  is required' });
-//         }
-//          if (!PAN ) {
-//             return res.json({ success: false, message: 'PAN  is required' });
-//         }
-        
-
-
-//         if (email) {
-//             const exists = await userModel.findOne({ email });
-//             if (exists) {
-//                 return res.json({ success: false, message: 'Email already exists' });
-//             }
-//             if (!validator.isEmail(email)) {
-//                 return res.json({ success: false, message: 'Invalid email' });
-//             }
-//         }
-
-//         if (phone) {
-//             const exists = await userModel.findOne({ phone });
-//             if (exists) {
-//                 return res.json({ success: false, message: 'Phone already exists' });
-//             }
-//         }
-
-//         if (password.length < 8) {
-//             return res.json({ success: false, message: " please enter a Strong Password " })
-//         }
-
-//         const salt = await bcrypt.genSalt(10);;
-//         const hashedPassword = await bcrypt.hash(password, salt);
-
-//         const sendrefferal = await generateReferralCode();
-
-        
-
-//          const newUser = new userModel({
-//             name,
-//             email,
-//             password: hashedPassword,
-//             phone,
-//             address,
-//             pinCode,
-//             district,
-//             city,
-//             country, 
-//             DOB, 
-//             PAN , 
-//             accountNumber : accountNumber || undefined , 
-//             IFSC : IFSC || undefined ,
-//             bankName : bankName || undefined  ,
-//             holderName: holderName || undefined ,
-//             sendrefferal ,
-//             inviterefferal 
-//         })
-
-//         const user = await newUser.save();
-//         const token = createToken(user._id);
-
-//         res.json({ success: true, message: "Registration Successfully ", token });
-
-
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: error.message });
-
-//     }
-// }
-
-
-
+// Registering User 
 const registerUser = async (req, res) => {
     try {
         const { name, email, password, phone, inviterefferal , pinCode  } = req.body;
-
-        
-
 
         if (!name) {
             return res.json({ success: false, message: 'Name is required' });
@@ -168,13 +61,6 @@ const registerUser = async (req, res) => {
                 return res.json({ success: false, message: 'Invalid email' });
             }
         }
-
-        // if(inviterefferal){
-        //     const exists = await userModel.findOne({ sendrefferal: inviterefferal });
-        //     if(!exists){
-        //         return  res.json({ success: false, message: ' Invite refferal is not valid' });
-        //     }
-        // }
 
         if (inviterefferal) {
             const inviter = await userModel.findOne({ sendrefferal: inviterefferal });
@@ -217,19 +103,6 @@ const registerUser = async (req, res) => {
             inviterefferal,
             pinCode,
             referalcount: 0
-
-
-            // address,
-            // pinCode,
-            // district,
-            // city,
-            // country, 
-            // DOB, 
-            // PAN , 
-            // accountNumber : accountNumber || undefined , 
-            // IFSC : IFSC || undefined ,
-            // bankName : bankName || undefined  ,
-            // holderName: holderName || undefined ,
         })
 
         const user = await newUser.save();
@@ -244,6 +117,8 @@ const registerUser = async (req, res) => {
 
     }
 }
+
+// Logging In user 
 
 const loginUser = async (req, res) => {
     try {
@@ -267,6 +142,7 @@ const loginUser = async (req, res) => {
 }
 
 
+//  fetch user 
 const fetchUser = async (req, res) => {
     try {
         const authHeader = req.headers.authorization;
@@ -301,6 +177,8 @@ const fetchUser = async (req, res) => {
     }
 };
 
+// Fetch All user 
+
 const allUser = async (req, res) => {
     try {
         const user = await userModel.find().lean();
@@ -318,6 +196,8 @@ const allUser = async (req, res) => {
     }
 }
 
+// Admin Login 
+
 const adminLogin = async (req,res) => {
   try {
     const { email, password } = req.body;
@@ -333,29 +213,7 @@ const adminLogin = async (req,res) => {
   }
 };
 
-// const refferals = async ( req, res)=>{
-//     try {
-//         const authHeader = req.headers.authorization;
-//         const token = authHeader && authHeader.split(' ')[1];
-//         let decoded;
-//         try {
-//             decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         } catch (error) {
-//             return res.status(401).json({ success: false, message: "Invalid or expired token" });
-//         }
-        
-//         if (!decoded?.id) {
-//             return res.status(401).json({ success: false, message: "Login again" });
-//         }
-
-//         const user = await userModel.findById(decoded.id).lean();
-//         if(user )
-
-        
-//     } catch (error) {
-        
-//     }
-// }
+// checking refferals 
 
 const refferals = async (req, res) => {
     try {
@@ -398,119 +256,11 @@ const refferals = async (req, res) => {
     }
 };
 
-
-
-// const updateUser = async (req, res) => {
-//     try {
-//         const { name, email, password, phone, address, pinCode, district , city, country , DOB , PAN , accountNumber , IFSC  , bankName , holderName ,  inviterefferal } = req.body;
-
-
-//         if (!name) {
-//             return res.json({ success: false, message: 'Name is required' });
-//         }
-//         if (!email) {
-//             return res.json({ success: false, message: 'Email is required' });
-//         }
-//         if (!password) {
-//             return res.json({ success: false, message: 'Password  is required' });
-//         }
-//         if (!phone) {
-//             return res.json({ success: false, message: 'Phone  is required' });
-//         }
-//         if (!address) {
-//             return res.json({ success: false, message: 'Address  is required' });
-//         }
-//         if (!pinCode) {
-//             return res.json({ success: false, message: 'Pincode is required' });
-//         }
-//         if (!city) {
-//             return res.json({ success: false, message: 'City is required' });
-//         }
-//         if (!district ) {
-//             return res.json({ success: false, message: 'District  is required' });
-//         }
-//          if (!country ) {
-//             return res.json({ success: false, message: 'Country  is required' });
-//         }
-
-//          if (!DOB) {
-//             return res.json({ success: false, message: 'DOB  is required' });
-//         }
-//          if (!PAN ) {
-//             return res.json({ success: false, message: 'PAN  is required' });
-//         }
-        
-
-
-//         if (email) {
-//             const exists = await userModel.findOne({ email });
-//             if (exists) {
-//                 return res.json({ success: false, message: 'Email already exists' });
-//             }
-//             if (!validator.isEmail(email)) {
-//                 return res.json({ success: false, message: 'Invalid email' });
-//             }
-//         }
-
-//         if (phone) {
-//             const exists = await userModel.findOne({ phone });
-//             if (exists) {
-//                 return res.json({ success: false, message: 'Phone already exists' });
-//             }
-//         }
-
-//         if (password.length < 8) {
-//             return res.json({ success: false, message: " please enter a Strong Password " })
-//         }
-
-//         const salt = await bcrypt.genSalt(10);;
-//         const hashedPassword = await bcrypt.hash(password, salt);
-
-//         const sendrefferal = await generateReferralCode();
-
-        
-
-//          const newUser = new userModel({
-//             name,
-//             email,
-//             password: hashedPassword,
-//             phone,
-//             address,
-//             pinCode,
-//             district,
-//             city,
-//             country, 
-//             DOB, 
-//             PAN , 
-//             accountNumber : accountNumber || undefined , 
-//             IFSC : IFSC || undefined ,
-//             bankName : bankName || undefined  ,
-//             holderName: holderName || undefined ,
-//             sendrefferal ,
-//             inviterefferal 
-//         })
-
-//         const user = await newUser.save();
-//         const token = createToken(user._id);
-
-//         res.json({ success: true, message: "Registration Successfully ", token });
-
-
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: error.message });
-
-//     }
-// }
-
-
-
+// Updating User 
 
 const updateUser = async (req, res) => {
   try {
-     // coming from token middleware
-    // const headers = req.header.authorization;
-    // const token = headers.split(' ')[1];
+
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
 
@@ -609,6 +359,7 @@ const updateUser = async (req, res) => {
   }
 };
 
+// Getting Dashboard Data 
 
 const getDashboardData = async (req, res) => {
     try {
@@ -641,6 +392,7 @@ const getDashboardData = async (req, res) => {
     }
 };
 
+// All Completed refferals
 
 const allCompletedrefferals = async (req, res) =>{
     try {
@@ -652,13 +404,80 @@ const allCompletedrefferals = async (req, res) =>{
     }
 }
 
-// const completedrefferal = async(req,res) =>{
-//     try {
-//         const 
-//     } catch (error) {
-        
-//     }
-// }
+const FRONTEND_URL = process.env.FRONTEND_URL; // Update as needed
+const MAIL_USER = process.env.MAIL_USER;
+const MAIL_PASS = process.env.MAIL_PASS;
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) return res.json({ success:false , message: 'User not found' });
+
+    const rawToken = crypto.randomBytes(32).toString('hex');
+    const hashedToken = crypto.createHash('sha256').update(rawToken).digest('hex');
+
+    user.resetToken = hashedToken;
+    user.resetTokenExpiry = Date.now() + 15 * 60 * 1000; // 15 minutes
+    await user.save();
+
+    const resetLink = `${FRONTEND_URL}/reset?token=${rawToken}`;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: { user: MAIL_USER, pass: MAIL_PASS },
+    });
+
+    await transporter.sendMail({
+      to: email,
+      subject: 'Reset Your Password',
+      html: `
+        <h3>Hello ${user.name || 'User'}</h3>
+        <p>Click the link below to reset your password:</p>
+        <a href="${resetLink}" target="_blank">${resetLink}</a>
+        <p>This link will expire in 15 minutes.</p>
+      `,
+    });
+
+    res.json({ success: true, message: 'Reset link sent to your email.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({sucess:false, message: 'Something went wrong' });
+  }
+};
+
+
+
+
+
+const resetPassword = async (req, res) => {
+  const { token, newPassword } = req.body;
+
+  try {
+    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    const user = await userModel.findOne({
+      resetToken: hashedToken,
+      resetTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) return res.json({ sucess:false , message: 'Token is invalid or expired' });
+
+    const salt = await bcrypt.genSalt(10);;
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashedPassword;
+    user.resetToken = undefined;
+    user.resetTokenExpiry = undefined;
+    await user.save();
+
+    res.json({ success:true , message: 'Password reset successful' });
+  } catch (error) {
+    console.error(error);
+    res.json({ success:false, message: 'Server error' });
+  }
+};
 
 
 
@@ -671,5 +490,7 @@ export {
     refferals,
     updateUser,
     getDashboardData,
-    allCompletedrefferals
+    allCompletedrefferals,
+    forgotPassword,
+    resetPassword
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { adminLogin, allCompletedrefferals, allUser, fetchUser, getDashboardData, loginUser, refferals, registerUser, updateUser } from '../controllers/userController.js';
+import { adminLogin, allCompletedrefferals, allUser, fetchUser, forgotPassword, getDashboardData, loginUser, refferals, registerUser, resetPassword, updateUser } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser);
 userRouter.post('/login', loginUser);
+userRouter.post('/forgot',forgotPassword);
+userRouter.post('/reset',resetPassword);
 userRouter.get('/fetch',fetchUser);
 userRouter.get('/alluser',allUser);
 userRouter.post('/adminlogin',adminLogin);
